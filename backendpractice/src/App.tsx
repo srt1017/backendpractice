b24efad4345dc8fe5020fce9a505aa33f82e28a4
srt1017 <omarfarouq107@gmail.com>
2023-05-22 14:56:37 -0500

    null

--- backendpractice/src/App.tsx
+++ backendpractice/src/App.tsx
@@ -1,7 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
+import axios, { CanceledError } from "axios";
 import { useEffect, useState } from "react";
- 
 
 interface User {
   id: number;
@@ -11,28 +10,47 @@ interface User {
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
-      .then((res) => setUsers(res.data))
-      .catch(err => setError(err.message));
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+      // .finally(() => {
+      //   setLoading(false)
+      // })
+
+    return () => controller.abort();
   }, []);
 
   return (
     <>
-    <p className="text-danger">{error}</p>
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.name}, {user.username}{" "}
-        </li>
-      ))}
-    </ul>
+      <p className="text-danger">{error}</p>
+      {isLoading && <div className="spinner-border"></div>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name}, {user.username}{" "}
+          </li>
+        ))}
+      </ul>
     </>
   );
-  
 }
 
 export default App;
