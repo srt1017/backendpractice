bfc3736c388dfff3902391cebe3d12d3d4b55484
srt1017 <omarfarouq107@gmail.com>
2023-05-22 14:24:05 -0500

    back end practice using axios

---
+++ backendpractice/src/App.tsx
@@ -0,0 +1,38 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+ 
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+}
+
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+      .then((res) => setUsers(res.data))
+      .catch(err => setError(err.message));
+  }, []);
+
+  return (
+    <>
+    <p className="text-danger">{error}</p>
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          {user.name}, {user.username}{" "}
+        </li>
+      ))}
+    </ul>
+    </>
+  );
+  
+}
+
+export default App;
