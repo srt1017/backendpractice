0f44c2915b18c5794637060584fa0aeff30fca95
srt1017 <omarfarouq107@gmail.com>
2023-05-22 17:05:26 -0500

    added functionality for updating, deleting, and creating data

--- backendpractice/src/App.tsx
+++ backendpractice/src/App.tsx
@@ -1,11 +1,10 @@
 import "bootstrap/dist/css/bootstrap.css";
-import axios, { CanceledError } from "axios";
 import { useEffect, useState } from "react";
+import apiClient, {CanceledError} from "./services/api-client";
 
 interface User {
   id: number;
   name: string;
-  username: string;
 }
 
 function App() {
@@ -18,8 +17,8 @@ function App() {
 
     setLoading(true);
 
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+    apiClient
+      .get<User[]>("/users", {
         signal: controller.signal,
       })
       .then((res) => {
@@ -31,21 +30,74 @@ function App() {
         setError(err.message);
         setLoading(false);
       });
-      // .finally(() => {
-      //   setLoading(false)
-      // })
+    // .finally(() => {
+    //   setLoading(false)
+    // })
 
     return () => controller.abort();
   }, []);
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    apiClient
+      .delete("/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 0, name: "mosh" };
+    setUsers([...users, newUser]);
+
+    apiClient
+      .post("/users", newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const updateUser = (user: User) => {
+    const originalUsers = [...users]
+    const updatedUser = { ...user, name: user.name + '!'};
+    setUsers(users.map(u => u.id === user.id ? updatedUser : u))
+
+    apiClient.patch('/users/' + user.id, updatedUser)
+      .catch(err => {
+        setError(err.message)
+        setUsers(originalUsers)
+      })
+  }
 
   return (
     <>
       <p className="text-danger">{error}</p>
       {isLoading && <div className="spinner-border"></div>}
-      <ul>
+      <button className="btn btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
+      <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id}>
-            {user.name}, {user.username}{" "}
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <div>
+              <button className="btn btn-outline-secondary mx-3" onClick={() => updateUser(user)}>Update</button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
